752d3715debb312af7a97b68f5638966ca83fbcd
Meezan Shaikh <heymeezan.shaikh@gmail.com>
2024-11-09 02:11:55 +0530

    api-integration

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-replaceText(document.body)
+// fetch bad words JSON from the hosted API
+fetch('https://meezan35.github.io/cussfreebrowsing/bad-words.json')
+  .then(response => response.json())
+  .then(badWordsJson => {
 
+    replaceText(document.body, badWordsJson);
+  })
+  .catch(error => {
+    console.error('There was an error fetching the bad words list:', error);
+  });
 
-
-function replaceText(element) {
+// Function to replace cuss words in the element text content
+function replaceText(element, badWordsJson) {
     if (element.hasChildNodes()) {
-      element.childNodes.forEach(replaceText);
+        element.childNodes.forEach(child => replaceText(child, badWordsJson));  // traversing DOM
     } else if (element.nodeType === Text.TEXT_NODE) {
-  
-      
-      const badWords = badWordsJson.RECORDS.map(record => record.word).join('|');
-      const re = new RegExp(badWords, "gi");
-  
-      if (element.textContent.match(re)) {
-        const newElement = document.createElement('span');
-        newElement.innerHTML = element.textContent.replace(re, 
-          '<span style="background-color:black;color:black;">cussword</span>');
-        element.replaceWith(newElement);
-      }
+        
+        const badWords = badWordsJson.RECORDS.map(record => record.word).join('|');
+        const re = new RegExp(badWords, "gi");
+        
+        if (element.textContent.match(re)) {
+            
+            const newElement = document.createElement('span');
+            newElement.innerHTML = element.textContent.replace(re, 
+                '<span style="background-color:black;color:black;">****</span>');
+            element.replaceWith(newElement);
+        }
     }
-  }
-  
-  replaceText(document.body);
+}
