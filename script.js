d95a2dcf52a396bf1862f05040235447f4a67b36
Meezan Shaikh <heymeezan.shaikh@gmail.com>
2024-11-09 01:52:27 +0530

    Added bad-words.json for cuss-free browsing

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 replaceText(document.body)
 
-function replaceText(element){
-    if(element.hasChildNodes()){
-        element.childNodes.forEach(replaceText)
-    }
-    else if(element.nodeType === Text.TEXT_NODE){
 
-        let cussWords = ["asshole","cunt","bastard","fuck","bitch","motherfucker","nigga","son of a bitch",];
-        let text = cussWords.join('|')  // bastard|offensive
-        var re = new RegExp(text, "gi")
-        if(element.textContent.match(re)){
-            const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(re,
-            '<span style="background-color:black;color:black;">cussword</span>')
-            element.replaceWith(newElement);
-        }
+
+function replaceText(element) {
+    if (element.hasChildNodes()) {
+      element.childNodes.forEach(replaceText);
+    } else if (element.nodeType === Text.TEXT_NODE) {
+  
+      
+      const badWords = badWordsJson.RECORDS.map(record => record.word).join('|');
+      const re = new RegExp(badWords, "gi");
+  
+      if (element.textContent.match(re)) {
+        const newElement = document.createElement('span');
+        newElement.innerHTML = element.textContent.replace(re, 
+          '<span style="background-color:black;color:black;">cussword</span>');
+        element.replaceWith(newElement);
+      }
     }
-}
+  }
+  
+  replaceText(document.body);
